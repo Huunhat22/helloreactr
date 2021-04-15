b0761ccda2dcf1afb67054edf58ff9b9f74dcc62
huunhat22 <huunhatth07@gmail.com>
2021-04-15 23:32:21 +0700

    New Project React Js App

---
+++ src/Demo.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Demo() {
+  return (
+    <div className="Demo">
+        <h1>Chào Ngày Mới</h1>
+        <p>Chúc bạn có một ngày mới thật nhiều năng lượng</p>
+    </div>
+  );
+}
+
+export default Demo;
